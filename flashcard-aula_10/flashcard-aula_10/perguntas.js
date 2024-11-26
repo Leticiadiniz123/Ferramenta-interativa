18b2fa551ef8145a395fd3911525b1cc3d093a9d
Leticiadiniz123 <leticiaadinizx@gmail.com>
2024-11-26 11:10:21 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Qual os meu cantores favoritos?',
+    'Chris Brown, Michael Jackson e The Weeknd'
+)
+
+criaCartao(
+    'Vida',
+    'Qual meu sonho?',
+    'Ter uma vida boa financeiramente e viajar o mundo'
+)
+
+criaCartao(
+    'Comida',
+    'Qual meu sabor favorito de bolo?',
+    'Chocolate'
+)
+
+criaCartao(
+    'Vida',
+    'O que eu mais gosto de fazer?',
+    'Estar com o meu amor, dançar, fazer unha, ouvir música'
+)
